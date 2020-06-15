374e1630d696f86663f0321621001a9d11f696fc
maddhruv <dhruvjainpenny@gmail.com>
2020-06-15 20:21:01 +0530

    feat: add more palettes

--- palettes/index.ts
+++ palettes/index.ts
@@ -9,7 +9,7 @@ const palette: Palette = {
   google: {
     name: "Google",
     colors: ["#008744", "#0057e7", "#d62d20", "#ffffff", "#ffa700"],
-    keywords: ["google"],
+    keywords: ["google", "google chrome"],
   },
   rainbow: {
     name: "Rainbow",
@@ -38,6 +38,83 @@ const palette: Palette = {
     name: "Table",
     colors: ["#D69E33", "#F6D756", "#E79126", "#784919", "#67390A"],
     keywords: ["table", "chair", "stool", "desk"],
+  },
+  whatsapp: {
+    name: "WhatsApp",
+    colors: ["#075E54", "#128C7E", "#25D366", "#DCF8C6", "#34B7F1", "#ECE5DD"],
+    keywords: ["whatsapp", "whats app", "facebook", "messenger"],
+  },
+  instagram: {
+    name: "Instagram",
+    colors: ["#F44747", "#EEDC31", "#7FDB6A", "#0E68CE"],
+    keywords: ["instagram", "facebook", "social network"],
+  },
+  pinterest: {
+    name: "Pinterest",
+    colors: ["#F0002A"],
+    keywords: ["pinterest", "social network"],
+  },
+  shopify: {
+    name: "Shopify",
+    colors: ["#64943E", "#95BF47", "#FFFFFF", "#000000"],
+    keywords: ["shopify", "shop"],
+  },
+  facebook: {
+    name: "Facebook",
+    colors: ["#3A5795", "#637BAD", "#ADB9D3", "#EDF0F5", "#333333"],
+    keywords: ["facebook", "face book", "social network"],
+  },
+  firefox: {
+    name: "Firefox",
+    colors: ["#58BEEC", "#074784", "#FFE513", "#EE8424", "#D65126"],
+    keywords: ["firefox", "browser", "mozilla", "mozilla firefox"],
+  },
+  iphone: {
+    name: "iPhone",
+    colors: ["#BDB08A", "#7C7C7C", "#313131", "#F2F1F2", "#A7A7A7"],
+    keywords: ["iphone", "i phone", "apple phone", "apple iphone"],
+  },
+  nvidia: {
+    name: "nVIDIA",
+    colors: ["#FFFFFF", "#333333", "#000000", "#6AB900", "#76B900"],
+    keywords: ["nvidia", "graphics"],
+  },
+  amd: {
+    name: "AMD",
+    colors: ["#EF0707", "#BD0000", "#FF2424", "#CA0505", "#FC0C0C"],
+    keywords: ["amd", "processor", "graphics"],
+  },
+  twitter: {
+    name: "Twitter",
+    colors: ["#326ada", "#d4d8d4", "#433e90", "#a19c9c", "#d2d2d2"],
+    keywords: ["twitter", "social network"],
+  },
+  icecream: {
+    name: "Ice Cream",
+    colors: ["#6b3e26", "#ffc5d9", "#c2f2d0", "#fdf5c9", "#ffcb85"],
+    keywords: ["ice cream", "icecream"],
+  },
+  fire: {
+    name: "Fire",
+    colors: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
+    keywords: ["fire", "agni", "flame", "burn"],
+  },
+  mickeymouse: {
+    name: "Mickey Mouse",
+    colors: ["#f9d70b", "#000000", "#f20505", "#ffffff", "#f0e046"],
+    keywords: ["mickey mouse", "minnie mouse", "mouse", "cartoon"],
+  },
+  ironman: {
+    name: "Iron Man",
+    colors: ["#771414", "#beba46", "#8bd3fb", "#bcb841", "#790d0d"],
+    keywords: [
+      "iron man",
+      "ironman",
+      "tony stark",
+      "robert downy jr",
+      "marvel",
+      "avengers",
+    ],
   },
 };
 
