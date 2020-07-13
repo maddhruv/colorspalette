4ebb526bb1ce8c39c53581f7db3a5bbcda456489
maddhruv <dhruvjainpenny@gmail.com>
2020-07-13 21:31:11 +0530

    fix: sort all palettes

--- palettes/index.ts
+++ palettes/index.ts
@@ -1,18 +1,224 @@
 import { Palette } from "./types";
 
 const palette: Palette = {
-  tree: {
-    name: "Tree",
-    colors: ["#40EE95", "#0CAE5B", "#613B16", "#A67B51"],
-    keywords: ["tree", "plant"],
+  amazon: {
+    colors: ["#FF9900", "#000000"],
+    keywords: ["amazon", "aws", "e-commerce"],
+    name: "Amazon",
+  },
+  amd: {
+    colors: ["#EF0707", "#BD0000", "#FF2424", "#CA0505", "#FC0C0C"],
+    keywords: ["amd", "processor", "graphics"],
+    name: "AMD",
+  },
+  apple: {
+    colors: ["#781714", "#EB4135", "#FEAD6A", "#FEF695", "#668014"],
+    keywords: ["apple", "fruit", "apple fruit"],
+    name: "Apple Fruit",
+  },
+  asus: {
+    colors: ["#00539B", "#FFFFFF"],
+    keywords: ["asus", "asus laptops"],
+    name: "ASUS",
+  },
+  avocado: {
+    colors: ["#4A7337", "#6B8C21", "#DDD48F", "#CDA989", "#704012"],
+    keywords: ["avocado", "fruit", "avocado fruit"],
+    name: "Avocado",
+  },
+  banana: {
+    colors: ["#FFFBC9", "#6B3E26", "#FBEC5D"],
+    keywords: ["banana", "fruit", "banana fruit"],
+    name: "Banana",
+  },
+  beach: {
+    colors: ["#1A748E", "#55C2D6", "#F0DF99", "#D38E31", "#992915"],
+    keywords: ["beach", "sea", "seashore", "ocean"],
+    name: "Beach",
+  },
+  birch: {
+    colors: ["#E3B581", "#F1C197", "#EFD0B5", "#F6DEC0", "#FFEBD0"],
+    keywords: ["birch", "wood", "tree"],
+    name: "Birch Wood",
+  },
+  blackberry: {
+    colors: ["#1E212E", "#261923", "#692536", "#EEF7FB", "#C8E2F0"],
+    keywords: ["blackberry", "fruit", "blackberry fruit"],
+    name: "Blackberry",
+  },
+  cherry: {
+    colors: ["#79011C", "#B6203B", "#FF0126", "#FBAAB9"],
+    keywords: ["cherry", "fruit", "cherry fruit"],
+    name: "Cherry",
+  },
+  coconut: {
+    colors: ["#4D331D", "#74533C", "#C59D7D", "#D0D0D8", "#EFEDF6"],
+    keywords: ["coconut", "fruit", "coconut fruit"],
+    name: "Coconut",
+  },
+  dell: {
+    colors: ["#007DB8", "#EFEFEF", "#FFFFFF", "#454545"],
+    keywords: ["dell", "dell computers "],
+    name: "dell",
+  },
+  ebay: {
+    colors: ["#E53238", "#0064D3", "#F5AF02", "#86B817"],
+    keywords: ["ebay", "e bay", "e-commerce"],
+    name: "EBay",
+  },
+  espressowood: {
+    colors: ["#231814", "#2D201B", "#432D25", "#35241B"],
+    keywords: ["espresso", "wood", "tree"],
+    name: "Espresso Wood",
+  },
+  facebook: {
+    colors: ["#3A5795", "#637BAD", "#ADB9D3", "#EDF0F5", "#333333"],
+    keywords: ["facebook", "face book", "social network"],
+    name: "Facebook",
+  },
+  fire: {
+    colors: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
+    keywords: ["fire", "agni", "flame", "burn"],
+    name: "Fire",
+  },
+  firefox: {
+    colors: ["#58BEEC", "#074784", "#FFE513", "#EE8424", "#D65126"],
+    keywords: ["firefox", "browser", "mozilla", "mozilla firefox"],
+    name: "Firefox",
+  },
+  flipkart: {
+    colors: ["#F7A200", "#F8E831", "#047BD5"],
+    keywords: ["flipkart", "flip kart", "e-commerce"],
+    name: "Flipkart",
+  },
+  github: {
+    colors: ["#333333", "#f5f5f5", "#4078c0"],
+    keywords: ["github", "git", "git hub"],
+    name: "GitHub",
   },
   google: {
-    name: "Google",
     colors: ["#008744", "#0057e7", "#d62d20", "#ffffff", "#ffa700"],
     keywords: ["google", "google chrome", "browser", "web browser"],
+    name: "Google",
+  },
+  gooseberry: {
+    colors: ["#DADFD0", "#7A8C66", "#778C0C", "#8E8158", "#848469"],
+    keywords: ["gooseberry", "fruit", "gooseberry fruit"],
+    name: "Gooseberry",
+  },
+  grape: {
+    colors: ["#89B061", "#8AB162", "#D9E8BF", "#EDEECE", "#C1DFC3"],
+    keywords: ["grape", "vine", "grapevine", "fruit", "grape fruit"],
+    name: "Grape",
+  },
+  grape2: {
+    colors: ["#BBC0E7", "#B89BE0", "#6546A7", "#571247", "#B477BA"],
+    keywords: ["grape", "vine", "grapevine", "fruit", "grape fruit"],
+    name: "Grape",
+  },
+  guava: {
+    colors: ["#2D2D2F", "#B0BC7C", "#618229", "#D6DD8F", "#F3B0A7"],
+    keywords: ["guava", "fruit", "guava fruit"],
+    name: "Guava",
+  },
+  icecream: {
+    colors: ["#6b3e26", "#ffc5d9", "#c2f2d0", "#fdf5c9", "#ffcb85"],
+    keywords: ["ice cream", "icecream"],
+    name: "Ice Cream",
+  },
+  instagram: {
+    colors: ["#F44747", "#EEDC31", "#7FDB6A", "#0E68CE"],
+    keywords: ["instagram", "facebook", "social network"],
+    name: "Instagram",
+  },
+  iphone: {
+    colors: ["#BDB08A", "#7C7C7C", "#313131", "#F2F1F2", "#A7A7A7"],
+    keywords: ["iphone", "i phone", "apple phone", "apple iphone"],
+    name: "iPhone",
+  },
+  ironman: {
+    colors: ["#771414", "#beba46", "#8bd3fb", "#bcb841", "#790d0d"],
+    keywords: [
+      "iron man",
+      "ironman",
+      "tony stark",
+      "robert downy jr",
+      "marvel",
+      "avengers",
+    ],
+    name: "Iron Man",
+  },
+  kiwi: {
+    colors: ["#120D0E", "#384D0E", "#82A546", "#D0E3A0", "#A48950", "#F7FBED"],
+    keywords: ["kiwi", "kiwifruit", "fruit", "kiwi fruit"],
+    name: "Kiwi",
+  },
+  lemon: {
+    colors: ["#D19402", "#FFDF30", "#FFEAA4", "#80A300", "#2D3F01"],
+    keywords: ["lemon", "lime", "fruit", "lemon fruit"],
+    name: "Lemon",
+  },
+  mango: {
+    colors: ["#FFCC22", "#FFD93A", "#FFEA47", "#FF8242"],
+    keywords: ["mango", "fruit", "mango fruit"],
+    name: "Mango",
+  },
+  maple: {
+    colors: ["#F1C38E", "#E7B983"],
+    keywords: ["maple", "maple wood", "wood", "tree"],
+    name: "Maple Wood",
+  },
+  mickeymouse: {
+    colors: ["#f9d70b", "#000000", "#f20505", "#ffffff", "#f0e046"],
+    keywords: ["mickey mouse", "minnie mouse", "mouse", "cartoon"],
+    name: "Mickey Mouse",
+  },
+  myntra: {
+    colors: ["#F13AB1", "#E72744", "#FD913C", "#F05524", "#29303E"],
+    keywords: ["myntra", "flipkart", "e-commerce"],
+    name: "Myntra",
+  },
+  nvidia: {
+    colors: ["#FFFFFF", "#333333", "#000000", "#6AB900", "#76B900"],
+    keywords: ["nvidia", "graphics"],
+    name: "nVIDIA",
+  },
+  olive: {
+    colors: ["#1C4015", "#54915F", "#9AD5D3"],
+    keywords: ["olive", "fruit", "olive fruit"],
+    name: "Olive",
+  },
+  oneplus: {
+    colors: ["#F7FEFA", "#F60414"],
+    keywords: ["oneplus", "one plus", "1+"],
+    name: "OnePlus",
+  },
+  orange: {
+    colors: ["#FFA809", "#FA7001", "#F8E9A4", "#F3CA10", "#6D9302"],
+    keywords: ["orange", "fruit", "orange fruit"],
+    name: "Orange",
+  },
+  paytm: {
+    colors: ["#002E6E", "#00B9F1"],
+    keywords: ["paytm", "pay tm", "online payment"],
+    name: "Paytm",
+  },
+  pear: {
+    colors: ["#6F5930", "#A4B650", "#E7E453", "#D99D26", "#B14934"],
+    keywords: ["pear", "fruit", "pear fruit"],
+    name: "Pear",
+  },
+  pineapple: {
+    colors: ["#7CB35C", "#557B3F", "#FFD964", "#C27815", "#A14712"],
+    keywords: ["pineapple", "pine apple", "fruit", "pineapple fruit"],
+    name: "Pineapple",
+  },
+  pinterest: {
+    colors: ["#F0002A"],
+    keywords: ["pinterest", "social network"],
+    name: "Pinterest",
   },
   rainbow: {
-    name: "Rainbow",
     colors: [
       "#ee82ee",
       "#4b0082",
@@ -23,268 +229,62 @@ const palette: Palette = {
       "#ff0000",
     ],
     keywords: ["rainbow", "rainbow colors"],
-  },
-  sea: {
-    name: "Sea",
-    colors: ["#E7F7EC", "#D4E8E9", "#A1CFFF", "#72D6EE", "#89BFFB", "#2DA0F9"],
-    keywords: ["sea", "ocean", "river", "water"],
-  },
-  beach: {
-    name: "Beach",
-    colors: ["#1A748E", "#55C2D6", "#F0DF99", "#D38E31", "#992915"],
-    keywords: ["beach", "sea", "seashore", "ocean"],
-  },
-  table: {
-    name: "Table",
-    colors: ["#D69E33", "#F6D756", "#E79126", "#784919", "#67390A"],
-    keywords: ["table", "chair", "stool", "desk"],
-  },
-  whatsapp: {
-    name: "WhatsApp",
-    colors: ["#075E54", "#128C7E", "#25D366", "#DCF8C6", "#34B7F1", "#ECE5DD"],
-    keywords: ["whatsapp", "whats app", "facebook", "messenger"],
-  },
-  instagram: {
-    name: "Instagram",
-    colors: ["#F44747", "#EEDC31", "#7FDB6A", "#0E68CE"],
-    keywords: ["instagram", "facebook", "social network"],
-  },
-  pinterest: {
-    name: "Pinterest",
-    colors: ["#F0002A"],
-    keywords: ["pinterest", "social network"],
-  },
-  shopify: {
-    name: "Shopify",
-    colors: ["#64943E", "#95BF47", "#FFFFFF", "#000000"],
-    keywords: ["shopify", "shop"],
-  },
-  facebook: {
-    name: "Facebook",
-    colors: ["#3A5795", "#637BAD", "#ADB9D3", "#EDF0F5", "#333333"],
-    keywords: ["facebook", "face book", "social network"],
-  },
-  firefox: {
-    name: "Firefox",
-    colors: ["#58BEEC", "#074784", "#FFE513", "#EE8424", "#D65126"],
-    keywords: ["firefox", "browser", "mozilla", "mozilla firefox"],
-  },
-  iphone: {
-    name: "iPhone",
-    colors: ["#BDB08A", "#7C7C7C", "#313131", "#F2F1F2", "#A7A7A7"],
-    keywords: ["iphone", "i phone", "apple phone", "apple iphone"],
-  },
-  nvidia: {
-    name: "nVIDIA",
-    colors: ["#FFFFFF", "#333333", "#000000", "#6AB900", "#76B900"],
-    keywords: ["nvidia", "graphics"],
-  },
-  amd: {
-    name: "AMD",
-    colors: ["#EF0707", "#BD0000", "#FF2424", "#CA0505", "#FC0C0C"],
-    keywords: ["amd", "processor", "graphics"],
-  },
-  twitter: {
-    name: "Twitter",
-    colors: ["#326ada", "#d4d8d4", "#433e90", "#a19c9c", "#d2d2d2"],
-    keywords: ["twitter", "social network"],
-  },
-  icecream: {
-    name: "Ice Cream",
-    colors: ["#6b3e26", "#ffc5d9", "#c2f2d0", "#fdf5c9", "#ffcb85"],
-    keywords: ["ice cream", "icecream"],
-  },
-  fire: {
-    name: "Fire",
-    colors: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
-    keywords: ["fire", "agni", "flame", "burn"],
-  },
-  mickeymouse: {
-    name: "Mickey Mouse",
-    colors: ["#f9d70b", "#000000", "#f20505", "#ffffff", "#f0e046"],
-    keywords: ["mickey mouse", "minnie mouse", "mouse", "cartoon"],
-  },
-  ironman: {
-    name: "Iron Man",
-    colors: ["#771414", "#beba46", "#8bd3fb", "#bcb841", "#790d0d"],
-    keywords: [
-      "iron man",
-      "ironman",
-      "tony stark",
-      "robert downy jr",
-      "marvel",
-      "avengers",
-    ],
-  },
-  oneplus: {
-    name: "OnePlus",
-    colors: ["#F7FEFA", "#F60414"],
-    keywords: ["oneplus", "one plus", "1+"],
-  },
-  samsung: {
-    name: "Samsung",
-    colors: ["#1428A0", "#2FB7E5", "#000000"],
-    keywords: ["samsung", "sam sung"],
-  },
-  dell: {
-    name: "dell",
-    colors: ["#007DB8", "#EFEFEF", "#FFFFFF", "#454545"],
-    keywords: ["dell", "dell computers "],
-  },
-  asus: {
-    name: "ASUS",
-    colors: ["#00539B", "#FFFFFF"],
-    keywords: ["asus", "asus laptops"],
-  },
-  apple: {
-    name: "Apple Fruit",
-    colors: ["#781714", "#EB4135", "#FEAD6A", "#FEF695", "#668014"],
-    keywords: ["apple", "fruit", "apple fruit"],
-  },
-  mango: {
-    name: "Mango",
-    colors: ["#FFCC22", "#FFD93A", "#FFEA47", "#FF8242"],
-    keywords: ["mango", "fruit", "mango fruit"],
-  },
-  banana: {
-    name: "Banana",
-    colors: ["#FFFBC9", "#6B3E26", "#FBEC5D"],
-    keywords: ["banana", "fruit", "banana fruit"],
-  },
-  pineapple: {
-    name: "Pineapple",
-    colors: ["#7CB35C", "#557B3F", "#FFD964", "#C27815", "#A14712"],
-    keywords: ["pineapple", "pine apple", "fruit", "pineapple fruit"],
-  },
-  pear: {
-    name: "Pear",
-    colors: ["#6F5930", "#A4B650", "#E7E453", "#D99D26", "#B14934"],
-    keywords: ["pear", "fruit", "pear fruit"],
-  },
-  avocado: {
-    name: "Avocado",
-    colors: ["#4A7337", "#6B8C21", "#DDD48F", "#CDA989", "#704012"],
-    keywords: ["avocado", "fruit", "avocado fruit"],
-  },
-  blackberry: {
-    name: "Blackberry",
-    colors: ["#1E212E", "#261923", "#692536", "#EEF7FB", "#C8E2F0"],
-    keywords: ["blackberry", "fruit", "blackberry fruit"],
-  },
-  cherry: {
-    name: "Cherry",
-    colors: ["#79011C", "#B6203B", "#FF0126", "#FBAAB9"],
-    keywords: ["cherry", "fruit", "cherry fruit"],
-  },
-  coconut: {
-    name: "Coconut",
-    colors: ["#4D331D", "#74533C", "#C59D7D", "#D0D0D8", "#EFEDF6"],
-    keywords: ["coconut", "fruit", "coconut fruit"],
-  },
-  gooseberry: {
-    name: "Gooseberry",
-    colors: ["#DADFD0", "#7A8C66", "#778C0C", "#8E8158", "#848469"],
-    keywords: ["gooseberry", "fruit", "gooseberry fruit"],
-  },
-  grape: {
-    name: "Grape",
-    colors: ["#89B061", "#8AB162", "#D9E8BF", "#EDEECE", "#C1DFC3"],
-    keywords: ["grape", "vine", "grapevine", "fruit", "grape fruit"],
-  },
-  grape2: {
-    name: "Grape",
-    colors: ["#BBC0E7", "#B89BE0", "#6546A7", "#571247", "#B477BA"],
-    keywords: ["grape", "vine", "grapevine", "fruit", "grape fruit"],
-  },
-  guava: {
-    name: "Guava",
-    colors: ["#2D2D2F", "#B0BC7C", "#618229", "#D6DD8F", "#F3B0A7"],
-    keywords: ["guava", "fruit", "guava fruit"],
-  },
-  kiwi: {
-    name: "Kiwi",
-    colors: ["#120D0E", "#384D0E", "#82A546", "#D0E3A0", "#A48950", "#F7FBED"],
-    keywords: ["kiwi", "kiwifruit", "fruit", "kiwi fruit"],
-  },
-  lemon: {
-    name: "Lemon",
-    colors: ["#D19402", "#FFDF30", "#FFEAA4", "#80A300", "#2D3F01"],
-    keywords: ["lemon", "lime", "fruit", "lemon fruit"],
-  },
-  olive: {
-    name: "Olive",
-    colors: ["#1C4015", "#54915F", "#9AD5D3"],
-    keywords: ["olive", "fruit", "olive fruit"],
-  },
-  orange: {
-    name: "Orange",
-    colors: ["#FFA809", "#FA7001", "#F8E9A4", "#F3CA10", "#6D9302"],
-    keywords: ["orange", "fruit", "orange fruit"],
-  },
-  github: {
-    name: "GitHub",
-    colors: ["#333333", "#f5f5f5", "#4078c0"],
-    keywords: ["github", "git", "git hub"],
-  },
-  flipkart: {
-    name: "Flipkart",
-    colors: ["#F7A200", "#F8E831", "#047BD5"],
-    keywords: ["flipkart", "flip kart", "e-commerce"],
-  },
-  amazon: {
-    name: "Amazon",
-    colors: ["#FF9900", "#000000"],
-    keywords: ["amazon", "aws", "e-commerce"],
-  },
-  paytm: {
-    name: "Paytm",
-    colors: ["#002E6E", "#00B9F1"],
-    keywords: ["paytm", "pay tm", "online payment"],
-  },
-  myntra: {
-    name: "Myntra",
-    colors: ["#F13AB1", "#E72744", "#FD913C", "#F05524", "#29303E"],
-    keywords: ["myntra", "flipkart", "e-commerce"],
-  },
-  ebay: {
-    name: "EBay",
-    colors: ["#E53238", "#0064D3", "#F5AF02", "#86B817"],
-    keywords: ["ebay", "e bay", "e-commerce"],
-  },
-  wordpress: {
-    name: "WordPress",
-    colors: ["#096484", "#4796B3", "#74B6CE", "#52ACCC"],
-    keywords: ["wordpress", "word press"],
-  },
-  redwood: {
-    name: "Redwood Finish Wood",
-    colors: ["#431811", "#8F432B", "#ECB069", "#763C2C", "#B74B25"],
-    keywords: ["redwood", "red wood", "wood", "tree"],
-  },
-  espressowood: {
-    name: "Espresso Wood",
-    colors: ["#231814", "#2D201B", "#432D25", "#35241B"],
-    keywords: ["espresso", "wood", "tree"],
-  },
-  standardwood: {
-    name: "Standard Wood",
-    colors: ["#DEB887", "#A55B53", "#5B270B"],
-    keywords: ["standard", "wood", "tree"],
-  },
-  maple: {
-    name: "Maple Wood",
-    colors: ["#F1C38E", "#E7B983"],
-    keywords: ["maple", "maple wood", "wood", "tree"],
-  },
-  birch: {
-    name: "Birch Wood",
-    colors: ["#E3B581", "#F1C197", "#EFD0B5", "#F6DEC0", "#FFEBD0"],
-    keywords: ["birch", "wood", "tree"],
+    name: "Rainbow",
   },
   redsandalwood: {
-    name: "Red Sandalwood",
     colors: ["#A45A52", "#DEA790", "#E3C4B0", "#DEB49C"],
     keywords: ["red sandal", "sandalwood", "wood", "tree"],
+    name: "Red Sandalwood",
+  },
+  redwood: {
+    colors: ["#431811", "#8F432B", "#ECB069", "#763C2C", "#B74B25"],
+    keywords: ["redwood", "red wood", "wood", "tree"],
+    name: "Redwood Finish Wood",
+  },
+  samsung: {
+    colors: ["#1428A0", "#2FB7E5", "#000000"],
+    keywords: ["samsung", "sam sung"],
+    name: "Samsung",
+  },
+  sea: {
+    colors: ["#E7F7EC", "#D4E8E9", "#A1CFFF", "#72D6EE", "#89BFFB", "#2DA0F9"],
+    keywords: ["sea", "ocean", "river", "water"],
+    name: "Sea",
+  },
+  shopify: {
+    colors: ["#64943E", "#95BF47", "#FFFFFF", "#000000"],
+    keywords: ["shopify", "shop"],
+    name: "Shopify",
+  },
+  standardwood: {
+    colors: ["#DEB887", "#A55B53", "#5B270B"],
+    keywords: ["standard", "wood", "tree"],
+    name: "Standard Wood",
+  },
+  table: {
+    colors: ["#D69E33", "#F6D756", "#E79126", "#784919", "#67390A"],
+    keywords: ["table", "chair", "stool", "desk"],
+    name: "Table",
+  },
+  tree: {
+    colors: ["#40EE95", "#0CAE5B", "#613B16", "#A67B51"],
+    keywords: ["tree", "plant"],
+    name: "Tree",
+  },
+  twitter: {
+    colors: ["#326ada", "#d4d8d4", "#433e90", "#a19c9c", "#d2d2d2"],
+    keywords: ["twitter", "social network"],
+    name: "Twitter",
+  },
+  whatsapp: {
+    colors: ["#075E54", "#128C7E", "#25D366", "#DCF8C6", "#34B7F1", "#ECE5DD"],
+    keywords: ["whatsapp", "whats app", "facebook", "messenger"],
+    name: "WhatsApp",
+  },
+  wordpress: {
+    colors: ["#096484", "#4796B3", "#74B6CE", "#52ACCC"],
+    keywords: ["wordpress", "word press"],
+    name: "WordPress",
   },
 };
 
